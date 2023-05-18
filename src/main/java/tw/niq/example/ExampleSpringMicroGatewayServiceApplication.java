120162e2d165566916976bb14114e25cfa9f497b
Nick <nick.chen@cmp.com.tw>
2023-05-18 10:57:52 +0800

    Initial commit

---
+++ src/main/java/tw/niq/example/ExampleSpringMicroGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package tw.niq.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringMicroGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringMicroGatewayServiceApplication.class, args);
+	}
+
+}
